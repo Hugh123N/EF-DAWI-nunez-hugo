8b4ca776bc91bd2a9250129ec2ce1b0edf9337cf
Hugh123N <hughhnp@gmail.com>
2024-12-15 12:28:29 -0500

    creacion de proyecto EF-DAWI CRUD API

---
+++ src/main/java/edu/i202221615/EF_Crud_Api/EfCrudApiApplication.java
@@ -0,0 +1,13 @@
+package edu.i202221615.EF_Crud_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/i202221615/EF_Crud_Api/EfCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.i202221615.EF_Crud_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
